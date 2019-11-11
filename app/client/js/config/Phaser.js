56ce81ca94c161e474406ec492027eeafe7071e0
informatykapalac <43820782+informatykapalac@users.noreply.github.com>
2019-11-11 20:29:02 +0100

    Update Phaser.js

--- app/client/js/config/Phaser.js
+++ app/client/js/config/Phaser.js
@@ -1,1 +1,23 @@
 //Plik zawierający konfigurację Phasera
+'use strict';
+import Screen from "Screen.js";
+//Importuje obiekt Screen z pliku Screen.js, w obiekcie tym znajduje się zmienna width i height
+
+const config = {
+    type: Phaser.AUTO,
+    width: Screen.width, //Szerokość gry przyjmuje rozmiar okienka
+    height: Screen.height, //Wysokość gry przyjmuje wysokość okienka
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+export default config;
