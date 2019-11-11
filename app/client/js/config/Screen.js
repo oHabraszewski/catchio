d9a57991885d365246dfa8fd6cb446c0f60b7603
informatykapalac <43820782+informatykapalac@users.noreply.github.com>
2019-11-11 20:45:42 +0100

    Fix do Screen.js

--- app/client/js/config/Screen.js
+++ app/client/js/config/Screen.js
@@ -1,5 +1,5 @@
 const Screen = {
-    width = document.body.clientWidth || window.innerWidth || document.documentElement.clientWidth,
-    height = document.body.clientHeight || window.innerHeight || document.documentElement.clientHeight
+    width : document.body.clientWidth || window.innerWidth || document.documentElement.clientWidth,
+    height : document.body.clientHeight || window.innerHeight || document.documentElement.clientHeight
 }
 export default Screen
