0b86aca737e9a9771e46c604fb8825c66cb4e115
Lukasz(Dyry) <thestrowg@gmail.com>
2019-11-12 15:54:50 +0100

    Stworzono strukture pliku Game.js

--- app/client/js/Game.js
+++ app/client/js/Game.js
@@ -1,1 +1,19 @@
 //Plik zawierający klasę sceny Game - naszej głównej sceny gry
+const Game extends Phaser.Scene = {
+
+    preload :function ()
+    {
+
+    },
+
+    create: function ()
+    {
+
+    },
+
+    update: function ()
+    {
+
+    }
+  }
+export default Game;
