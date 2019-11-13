c12e9462898b2ec668e0c5d6764d2642ab5d5d92
Oskar <oskarek.i.tomirek@gmail.com>
2019-11-13 18:44:04 +0100

    SCSS

--- app/client/app.js
+++ app/client/app.js
@@ -1,4 +1,5 @@
 'use strict';
+import styles from "./scss/index.scss" //Getting styles in scss
 import Phaser from 'phaser';
 import config from "./js/config/Phaser.js";
 import Game from "./js/Game.js";
