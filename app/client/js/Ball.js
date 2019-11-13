ab6ec5df8fb49fb654a21e508520592f1037dbe7
Lukasz(Dyry) <thestrowg@gmail.com>
2019-11-13 18:50:26 +0100

    Szkic Ball.js

--- app/client/js/Ball.js
+++ app/client/js/Ball.js
@@ -1,1 +1,9 @@
 //Plik zawierający klasę piłki - obiektu który będziemy kraść i rzucać do bramek
+class Ball extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, texture) {
+        super(scene, x, y, texture);
+        // ...
+        scene.add.existing(this);
+    }
+    
+}
