55f9d78147c35e97abfe76454ee92d2f7aa83bfd
Lukasz(Dyry) <thestrowg@gmail.com>
2019-11-13 18:32:17 +0100

    deletion of comments

--- app/client/app.js
+++ app/client/app.js
@@ -1,18 +1,8 @@
 'use strict';
 import Phaser from 'phaser';
-/* develblock:start */
+
 import config from "./js/config/Phaser.js";
 import Game from "./js/Game.js";
 
 const game = new Phaser.Game(config);
 console.log(`Only in development`)
-
-/* develblock:end */
-
-// require('./js/game/game.js')
-
-// const Phaser = require('phaser')
-// const config = require('./config/phaserConfig.js')
-
-
-// const world = new Phaser.Game(config)
