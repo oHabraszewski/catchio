e00fc19e5ad3d89ca8f38cdbf3ac9f9afeb51afa
Lukasz(Dyry) <thestrowg@gmail.com>
2019-11-14 22:06:46 +0100

    Poprawa klasy piłki i test importu obrazka

--- app/client/js/Ball.js
+++ app/client/js/Ball.js
@@ -1,9 +1,13 @@
 //Plik zawierający klasę piłki - obiektu który będziemy kraść i rzucać do bramek
+'use strict';
+import Phaser from 'phaser';
+
 class Ball extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture) {
         super(scene, x, y, texture);
         // ...
         scene.add.existing(this);
     }
-    
+
 }
+export default Ball;
--- app/client/js/Game.js
+++ app/client/js/Game.js
@@ -1,5 +1,6 @@
 //Plik zawierający klasę sceny Game - naszej głównej sceny gry
 import Phaser from 'phaser';
+//import Ball from './Ball.js'; - pozniej sprawdze
 
 class Game extends Phaser.Scene {
     constructor (setup) {
@@ -7,11 +8,11 @@ class Game extends Phaser.Scene {
     }
 
     preload(){
-
+          this.load.image('ball', "../../assets/img/ball.png");
     }
 
     create(){
-
+      this.add.image(400, 300, 'ball'); // test czy działa import obrazka
     }
 
     update(){
