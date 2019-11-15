f2529ccf81dc77d2dfd2a17429d2e5ac44b69d8f
Wolmin <lamonos123@gmail.com>
2019-11-15 18:40:38 +0100

    Update Player.js

--- app/client/js/Player.js
+++ app/client/js/Player.js
@@ -1,4 +1,7 @@
 //Plik zawierający ogólną klasę gracza
+'use strict';
+import Phaser from 'phaser';
+
 class Player extends Phaser.GameObjects.Sprite() {
     constructor (scene, x, y, texture) {
         super(scene, x, y, texture);
@@ -6,3 +9,4 @@ class Player extends Phaser.GameObjects.Sprite() {
         scene.add.existing(this);
     }
 }
+export default Player;
