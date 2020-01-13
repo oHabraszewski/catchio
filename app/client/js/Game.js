4eb17977706cfd1fa9c4a436506d9a84d8e331b5
Oskar <oskarek.i.tomirek@gmail.com>
2020-01-13 21:02:21 +0100

    Lekki bałagan, ale mamy mapę wraz z kolizjami

--- app/client/js/Game.js
+++ app/client/js/Game.js
@@ -14,17 +14,28 @@ class Game extends Phaser.Scene {
         super(setup);
     }
     preload(){
+          this.load.image("tiles", "../../assets/img/tiles.png");
+          this.load.tilemapCSV("map", "../../assets/img/map.csv")
+          //this.load.image("mapIMG", "../../assets/img/tiles.png")
+          //this.load.tilemapTiledJSON("map", "../../assets/img/map1.json");
           this.load.image('ball', "../../assets/img/ball.png");
           this.load.image('player0', "../../assets/img/player0.png");
           this.load.image('player1', "../../assets/img/player1.png");
     }
 
     create(){
+      const map = this.make.tilemap({ key: "map", tileWidth: 40, tileHeight: 40 });
+      const tileset = map.addTilesetImage("tiles");
+      const layer = map.createStaticLayer(0, tileset,0,0);
+      layer.setCollision([3,0])
       ball = new Ball(this, 400, 300,'ball');
       player1 = new Player(this, 1000, 500, 'player0');
       player2 = new Player(this, 300, 500, 'player1');
       overlapCollider = this.physics.add.overlap(ball, player1, ball.getOwner);
       overlapCollider2 = this.physics.add.overlap(ball, player2, ball.getOwner);
+      this.physics.add.collider(player1, layer);
+      this.physics.add.collider(player2, layer);
+      this.physics.add.collider(ball, layer);
 
     }
 
