b5f48729459dc9c28c9ff8dac1e2cae6ca3ab9f3
Wiktor <wiktor.jurkiewicz.wiktor@gmail.com>
2020-01-25 12:17:13 +0100

    merge

--- app/client/index.js
+++ app/client/index.js
@@ -3,30 +3,32 @@ require('./src/scss/index.scss') // Getting styles in scss
 import Phaser from 'phaser';
 import config from "./src/js/config/Phaser";
 
-const   mainDiv = document.getElementById("main"),
-        gameDiv = document.getElementById("game"),
-        creditsDiv = document.getElementById("credits"),
-        mainBut = document.getElementById("toMainCredits"),
-        gameBut = document.getElementById("play"),
-        creditsBut = document.getElementById("crediting")
+const mainDiv = document.getElementById("main");
+const gameDiv = document.getElementById("game");
+const creditsDiv = document.getElementById("credits");
+const mainBut = document.getElementById("toMainCredits");
+const gameBut = document.getElementById("play");
+const creditsBut = document.getElementById("crediting");
 
-mainBut.onclick = ()=>{toMain()}
-gameBut.onclick = ()=>{toGame()}
-creditsBut.onclick = ()=>{toCredits()}
-function toCredits(){
+mainBut.onclick = () => { toMain() }
+gameBut.onclick = () => { toGame() }
+creditsBut.onclick = () => { toCredits() }
+
+function toCredits() {
     mainDiv.style.visibility = 'hidden'
     gameDiv.style.visibility = 'hidden'
     creditsDiv.style.visibility = 'visible'
 }
-function toGame(){
+function toGame() {
     mainDiv.style.visibility = 'hidden'
     gameDiv.style.visibility = 'visible'
     creditsDiv.style.visibility = 'hidden'
     new Phaser.Game(config);
 }
-function toMain(){
+function toMain() {
     mainDiv.style.visibility = 'visible'
     gameDiv.style.visibility = 'hidden'
     creditsDiv.style.visibility = 'hidden'
 }
+
 toMain()
